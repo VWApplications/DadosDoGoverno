80c886aa4345d1f732baf77bbcae2a912b413fbf
Victor Arnaud <victor_hugo_ad@hotmail.com>
2015-10-30 20:21:31 -0200

    Interface das listas

--- src/views/ListaPartidos.java
+++ src/views/ListaPartidos.java
@@ -9,12 +9,12 @@ package views;
  *
  * @author victor
  */
-public class ListaDeputados extends javax.swing.JFrame {
+public class ListaPartidos extends javax.swing.JFrame {
 
     /**
      * Creates new form ListaPartidos
      */
-    public ListaDeputados() {
+    public ListaPartidos() {
         initComponents();
     }
 
@@ -27,26 +27,27 @@ public class ListaDeputados extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jInternalFrameListaDeputados = new javax.swing.JInternalFrame();
-        jScrollPaneListaDeputados = new javax.swing.JScrollPane();
-        jTableListaDeputados = new javax.swing.JTable();
+        jInternalFrameListaPartidos = new javax.swing.JInternalFrame();
+        jScrollPaneListarPartidos = new javax.swing.JScrollPane();
+        jTableListarPartidos = new javax.swing.JTable();
+        jTextFieldPesquisa = new javax.swing.JTextField();
         jComboBoxOrdem = new javax.swing.JComboBox();
-        jTextFieldPesquisar = new javax.swing.JTextField();
         jButtonPesquisar = new javax.swing.JButton();
         jButtonFechar = new javax.swing.JButton();
         jLabelFundo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(630, 487));
+        setMaximumSize(new java.awt.Dimension(630, 480));
+        setPreferredSize(new java.awt.Dimension(630, 478));
         setResizable(false);
         setSize(new java.awt.Dimension(500, 500));
         getContentPane().setLayout(null);
 
-        jInternalFrameListaDeputados.setTitle("Lista de Deputados");
-        jInternalFrameListaDeputados.setVisible(true);
-        jInternalFrameListaDeputados.getContentPane().setLayout(null);
+        jInternalFrameListaPartidos.setTitle("Deputados por Partido");
+        jInternalFrameListaPartidos.setVisible(true);
+        jInternalFrameListaPartidos.getContentPane().setLayout(null);
 
-        jTableListaDeputados.setModel(new javax.swing.table.DefaultTableModel(
+        jTableListarPartidos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {},
                 {},
@@ -57,33 +58,33 @@ public class ListaDeputados extends javax.swing.JFrame {
 
             }
         ));
-        jScrollPaneListaDeputados.setViewportView(jTableListaDeputados);
+        jScrollPaneListarPartidos.setViewportView(jTableListarPartidos);
 
-        jInternalFrameListaDeputados.getContentPane().add(jScrollPaneListaDeputados);
-        jScrollPaneListaDeputados.setBounds(0, 262, 620, 190);
+        jInternalFrameListaPartidos.getContentPane().add(jScrollPaneListarPartidos);
+        jScrollPaneListarPartidos.setBounds(0, 252, 620, 200);
+        jInternalFrameListaPartidos.getContentPane().add(jTextFieldPesquisa);
+        jTextFieldPesquisa.setBounds(200, 150, 220, 27);
 
-        jComboBoxOrdem.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Ordem Alfabetica", "Ordem por Partido", "Ordem por Estado" }));
-        jInternalFrameListaDeputados.getContentPane().add(jComboBoxOrdem);
-        jComboBoxOrdem.setBounds(190, 20, 260, 27);
-        jInternalFrameListaDeputados.getContentPane().add(jTextFieldPesquisar);
-        jTextFieldPesquisar.setBounds(200, 150, 220, 27);
+        jComboBoxOrdem.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Ordem Alfabetica", " " }));
+        jInternalFrameListaPartidos.getContentPane().add(jComboBoxOrdem);
+        jComboBoxOrdem.setBounds(190, 10, 260, 27);
 
         jButtonPesquisar.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
         jButtonPesquisar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/pesquisar.png"))); // NOI18N
         jButtonPesquisar.setText("PESQUISAR");
-        jInternalFrameListaDeputados.getContentPane().add(jButtonPesquisar);
+        jInternalFrameListaPartidos.getContentPane().add(jButtonPesquisar);
         jButtonPesquisar.setBounds(200, 180, 220, 70);
 
         jButtonFechar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Fechar.png"))); // NOI18N
-        jInternalFrameListaDeputados.getContentPane().add(jButtonFechar);
+        jInternalFrameListaPartidos.getContentPane().add(jButtonFechar);
         jButtonFechar.setBounds(580, 0, 40, 40);
 
         jLabelFundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/debate.jpg"))); // NOI18N
-        jInternalFrameListaDeputados.getContentPane().add(jLabelFundo);
-        jLabelFundo.setBounds(0, -4, 620, 460);
+        jInternalFrameListaPartidos.getContentPane().add(jLabelFundo);
+        jLabelFundo.setBounds(0, 0, 620, 450);
 
-        getContentPane().add(jInternalFrameListaDeputados);
-        jInternalFrameListaDeputados.setBounds(0, 0, 630, 480);
+        getContentPane().add(jInternalFrameListaPartidos);
+        jInternalFrameListaPartidos.setBounds(0, 0, 630, 482);
 
         pack();
         setLocationRelativeTo(null);
@@ -106,21 +107,20 @@ public class ListaDeputados extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ListaDeputados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListaPartidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ListaDeputados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListaPartidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ListaDeputados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListaPartidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ListaDeputados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListaPartidos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ListaDeputados().setVisible(true);
+                new ListaPartidos().setVisible(true);
             }
         });
     }
@@ -129,10 +129,10 @@ public class ListaDeputados extends javax.swing.JFrame {
     private javax.swing.JButton jButtonFechar;
     private javax.swing.JButton jButtonPesquisar;
     private javax.swing.JComboBox jComboBoxOrdem;
-    private javax.swing.JInternalFrame jInternalFrameListaDeputados;
+    private javax.swing.JInternalFrame jInternalFrameListaPartidos;
     private javax.swing.JLabel jLabelFundo;
-    private javax.swing.JScrollPane jScrollPaneListaDeputados;
-    private javax.swing.JTable jTableListaDeputados;
-    private javax.swing.JTextField jTextFieldPesquisar;
+    private javax.swing.JScrollPane jScrollPaneListarPartidos;
+    private javax.swing.JTable jTableListarPartidos;
+    private javax.swing.JTextField jTextFieldPesquisa;
     // End of variables declaration//GEN-END:variables
 }
